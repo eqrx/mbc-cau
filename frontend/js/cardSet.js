763c23906fb5a3f3224ecf1fffa06fd1f5ac94ff
Akardian <mike.wuestenberg@haw-hamburg.de>
2015-12-30 23:54:47 +0100

    snaphot

--- frontend/js/cardSet.js
+++ frontend/js/cardSet.js
@@ -25,21 +25,23 @@ var CardSet = {
     
     //Ändert denn Text auf einer Karte
     cardUpdate: function(cards, next) {
-        $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () { //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
-            $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
-            $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
+        if (cards.length > 0) {
+            $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () { //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
+                $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
+                $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
+                
+                 next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
+                if( next.nextCard >= cards.length) {
+                    next.nextCard = 0;
+                }
+                   
+                next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
+                if(next.nextPanel >= sCardSet.maxPanels) {
+                    next.nextPanel = 0;
+                }
+            }).fadeIn(sCardSet.fadeTime);
             
-             next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
-            if( next.nextCard >= cards.length) {
-                next.nextCard = 0;
-            }
-               
-            next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
-            if(next.nextPanel >= sCardSet.maxPanels) {
-                next.nextPanel = 0;
-            }
-        }).fadeIn(sCardSet.fadeTime);
-        
-        return next;
+            return next;
+        }
     }
 };
