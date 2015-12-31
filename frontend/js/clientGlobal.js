7f2ee67d7e3d0451dad5cd8154a9184df39ba6e2
Akardian <mike.wuestenberg@haw-hamburg.de>
2015-12-31 01:09:27 +0100

    snaphot

--- frontend/js/clientGlobal.js
+++ frontend/js/clientGlobal.js
@@ -3,7 +3,7 @@ $(function() {
     var cardSet = new Array();
 	var voteCard = new Array();
     var duration;
-    var playerName = "";
+    var playerName = "Max";
     var next = new Place(0, 0);
 	
     /*cards.push(new Card("Dwayna", 10));
