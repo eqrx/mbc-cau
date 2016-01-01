980440fb888117fb06a7e32f80131064cab7af4a
Akardian <mike.wuestenberg@haw-hamburg.de>
2016-01-02 00:22:39 +0100

    sockets

--- frontend/js/clientGlobal.js
+++ frontend/js/clientGlobal.js
@@ -12,26 +12,13 @@ $(function() {
     cards.push(new Card("Melandru2", 7));
     cards.push(new Card("Lyssa-LANGER SUPER TEXT ZUM TESTEN2", 5));
     cards.push(new Card("Grenth2", 3));
-    cards.push(new Card("Kormir2", 1));
-	
-	voteCard.push(cards[0].name);
-	voteCard.push(cards[5].name);
-	voteCard.push(cards[2].name);
-	voteCard.push(cards[3].name);*/
-    
-	//alert(voteCard[0]);
-
+    cards.push(new Card("Kormir2", 1));*/
     
     CardSet.init();
     WhiteCard.init();
     Highscore.init(cardSet);
     ScoreName.init();
     Socket.init();
-
-	//WhiteCard.cardUpdate(voteCard);
-    //WhiteCard.cardShow();
-    
-    //Socket.bindVoteButtons();
     
     var cardSetTimer = setInterval(function() {CardSet.cardUpdate(cardSet, next) }, CardSet.settings.updateTime);    
 });
