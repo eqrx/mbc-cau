d9a9aaf452f04ddaac8473cb2410e24da419942a
Akardian <mike.wuestenberg@haw-hamburg.de>
2016-01-02 11:37:48 +0100

    sockets

--- frontend/js/cardSet.js
+++ frontend/js/cardSet.js
@@ -56,39 +56,34 @@ var CardSet = {
         
         if ((cards.length > 0 /*&& next.count <=  cards.length) || cards.length >= sCardSet.*maxPanels*/)) {
             for(var i = 0; i < sCardSet.maxPanels; i++) {
+                console.log($(sCardSet.text + i).html());
                 if(cards[next.nextCard].name == $(sCardSet.text + i).html()) {
                     $(sCardSet.vote + i).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
                     isInPanel = true;
                 }
             }
-        }
         
-        if(isInPanel == false) {
-            //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
-            $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () { next = CardSet.cardUpdateHelper(cards, next);
-            }).fadeIn(sCardSet.fadeTime);
+        
+            if(isInPanel == false) {
+                //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
+                $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () {                    
+                    $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
+                    $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
+                    
+                    next.count++;
+                    next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
+                    if( next.nextCard >= cards.length) {
+                        next.nextCard = 0;
+                    }
+                       
+                    next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
+                    if(next.nextPanel >= sCardSet.maxPanels) {
+                        next.nextPanel = 0;
+                    }
+                }).fadeIn(sCardSet.fadeTime);
+            }
         }
         return next;
-    },
-    
-    cardUpdateHelper: function (cards, next) {
-        console.log(cards);
-        console.log(next);
-        console.log(cards[next.nextCard].name);
-        
-        $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
-        $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
-        
-        next.count++;
-        next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
-        if( next.nextCard >= cards.length) {
-            next.nextCard = 0;
-        }
-           
-        next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
-        if(next.nextPanel >= sCardSet.maxPanels) {
-            next.nextPanel = 0;
-        }
     },
     
     //Speichert das Update der gespielten Karten ins array
