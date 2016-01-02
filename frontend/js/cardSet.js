26c4ef5c9b3f373fd4c853386553df0683e43e38
Akardian <mike.wuestenberg@haw-hamburg.de>
2016-01-02 11:24:28 +0100

    sockets

--- frontend/js/cardSet.js
+++ frontend/js/cardSet.js
@@ -62,26 +62,30 @@ var CardSet = {
             }
         }
         
-        if( isInPanel == false) {
+        if(isInPanel == false) {
             console.log("CardsSet: cardUpdate")
             console.log(cards);
-            $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () { //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
-                $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
-                $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
-                
-                next.count++;
-                next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
-                if( next.nextCard >= cards.length) {
-                    next.nextCard = 0;
-                }
-                   
-                next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
-                if(next.nextPanel >= sCardSet.maxPanels) {
-                    next.nextPanel = 0;
-                }
+            //Durch das aufrufen der Funktion wird Sichergestellt das der Inhalt duchgeführt wird bevor das fadeIn passiert
+            $(sCardSet.panel + next.nextPanel).fadeOut(sCardSet.fadeTime, function () { next = CardsSet.cardUpdateHelper(cards, next);
             }).fadeIn(sCardSet.fadeTime);
         }
         return next;
+    },
+    
+    cardUpdateHelper: function (cards, next) {
+        $(sCardSet.text + next.nextPanel).html(cards[next.nextCard].name); //Verändert denn Text der Karte
+        $(sCardSet.vote + next.nextPanel).html("Votes: " + cards[next.nextCard].votes); //Verändert die Votes der Karte
+        
+        next.count++;
+        next.nextCard++; //Auswahl der Nächsten Karte ausgegeben werden soll
+        if( next.nextCard >= cards.length) {
+            next.nextCard = 0;
+        }
+           
+        next.nextPanel++; //Auswahl des Nächsten Panels welches verändert werden soll
+        if(next.nextPanel >= sCardSet.maxPanels) {
+            next.nextPanel = 0;
+        }
     },
     
     //Speichert das Update der gespielten Karten ins array
